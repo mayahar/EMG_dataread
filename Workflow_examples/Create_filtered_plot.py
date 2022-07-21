a5b6d661183fe2813bd038dd0e11fd0b14e395a9
Maya <maya11harel@gmail.com>
2022-07-21 16:54:59 +0300

    Added exampels

--- Workflow_examples/Create_filtered_plot.py
+++ Workflow_examples/Create_filtered_plot.py
@@ -6,6 +6,7 @@ subject.all_electrodes_plot()
 for muscle in subject.labels:
     subject.high_pass_filter(muscle)
 subject.high_pass_filter("Stomach",70)
+#The filter needed to compensate for hearbits interfering with the signal
 subject.all_electrodes_plot()
 
 subject.rms()
