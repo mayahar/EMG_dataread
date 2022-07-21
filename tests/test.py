66bfd6456dbe6d092ff0de8ade711acca99d6efc
Maya <maya11harel@gmail.com>
2022-07-21 19:27:36 +0300

    Added a relvent test to the code

---
+++ tests/test.py
@@ -0,0 +1,22 @@
+from AGSM.classes import EMG
+
+def check_ref_label_removel():
+    labels = ["Biceps","ref","Triceps"]
+    filtered_labels = ["Biceps","Triceps"]
+    q = EMG(labels)
+    assert filtered_labels == q.labels
+
+
+if __name__ == "__main__":
+    test_functions = ["check_ref_label_removel"]
+    errors = []
+
+    for func in test_functions:
+        try:
+            eval(func)()
+        except Exception as e:
+            errors.append(f"Failed when testing method '{func}': {e}")
+    if len(errors) > 0:
+        print(errors)
+    else:
+        print("Tests pass successfully.")
